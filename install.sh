fa415d2454c0845a4888c95a888c06b9f00a8b75
James Holmes <jdh@rtds.com>
2021-05-31 15:34:40 -0500

    Renamed groupId in install.sh to org.apache.shiro.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn install:install-file -DgroupId=rtds -DartifactId=shiro-faces -Dversion=2.0.1 -Dfile=target/shiro-faces-2.0.1.jar -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -DlocalRepositoryPath=./repo/
+mvn install:install-file -DgroupId=org.apache.shiro -DartifactId=shiro-faces -Dversion=2.0.1 -Dfile=target/shiro-faces-2.0.1.jar -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -DlocalRepositoryPath=./repo/
 
